c29fc4e18f5e66b28c3b0dc11f6cd27cd0d71a1b
Tahsin Yigit <tahsinyigit6h@gmail.com>
2024-11-12 13:07:22 +0300

    added some double dots to make the code prettier

--- pyton örnkeleri/ornek1.py
+++ pyton örnkeleri/ornek1.py
@@ -6,11 +6,11 @@ counter=0
 while (counter<=maxtry):    
     userinput=int(input("guess the number"))
     if userinput==n1 and counter<=maxtry:
-        print("congras you have won now you are gay")
+        print("congras you have won now you are gay:")
     elif userinput>n1 and counter<=maxtry:
-        print("too high you have to go lower")
+        print("too high you have to go lower:")
     elif userinput<n1 and counter<=maxtry:
-        print("too low you have to go higer")
+        print("too low you have to go higer:")
     counter=counter+1       
 
   
