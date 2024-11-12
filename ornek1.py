54b0f9daacd7cfdabac308925731012d30e7c94a
Tahsin Yigit <tahsinyigit6h@gmail.com>
2024-11-12 12:52:27 +0300

    sayı tahmin etme oyunu

---
+++ ornek1.py
@@ -0,0 +1,16 @@
+import random
+
+n1=random.randrange(100)
+maxtry=7
+counter=0
+while (counter<=maxtry):    
+    userinput=int(input("guess the number"))
+    if userinput==n1 and counter<=maxtry:
+        print("congrats you have won now you are gay")
+    elif userinput>n1 and counter<=maxtry:
+        print("too high you have to go lower")
+    elif userinput<n1 and counter<=maxtry:
+        print("too low you have to go higer")
+    counter=counter+1       
+
+  
